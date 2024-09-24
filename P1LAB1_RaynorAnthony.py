c757bc02c2262b9de3ae8f804969d2fe4b563a94
Anthony <ajr320@outlook.com>
2024-09-24 14:21:20 -0400

    First upload

---
+++ P1LAB1_RaynorAnthony.py
@@ -0,0 +1,15 @@
+# Anthony Raynor
+# 9/24/2024
+# P1LAB1
+# get input from the user and display output
+
+#Get first name from user
+firstName = input('Enter your first name: ')
+print()
+#Get lastt name from user
+lastName = input('Enter your last name: ')
+print()
+#Display info to user 
+print('Hello,', firstName, lastName + "! Welcome to CTI-100")                
+
+                 
