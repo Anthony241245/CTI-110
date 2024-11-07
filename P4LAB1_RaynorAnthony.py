bafd9026065f2daa77d9d9983053778be35943f9
Anthony <ajr320@outlook.com>
2024-11-07 14:38:52 -0500

    Add files via upload

---
+++ P4LAB1_RaynorAnthony.py
@@ -0,0 +1,32 @@
+# Anthony Raynor
+# 11/7/24
+# Draw a sqaure amd triangle using turtle library
+# Only need 1 turtle
+
+import turtle
+win = turtle.Screen()
+t = turtle.Turtle()
+win.bgcolor("black")
+
+t.pensize(10)                   # increase pensize(takes integer)
+t.pencolor("red")            # set pencolor(takes string)
+t.shape("arrow")
+
+
+
+# While loop to draw a square
+num = 0
+while num  < 4:
+    t.forward(150)
+    t.right(90)
+    num += 1
+print("while loop ends")
+
+# For loop to draw a triangle
+for tr in range(0,3):
+    t.forward(150)
+    t.left(120)
+print("For loop has ended")
+
+
+
