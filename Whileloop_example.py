fb5bd2c2edd849cff97c6a68ee8681c856a08e9b
Anthony <ajr320@outlook.com>
2024-11-07 14:07:05 -0500

    Add files via upload

---
+++ Whileloop_example.py
@@ -0,0 +1,24 @@
+# While loop runs until condition is met
+# A while loop will always be true or false
+# num = num+1 is the same as num +=1
+# While loop examples
+
+
+'''
+run_again = input("Enter 'run' to run the program or 'exit' to end: ")
+
+while run_again.lower() != "exit":
+    print("Do something...")
+    run_again = input("Enter 'yes' to run or 'exit' to end: ")
+# Loop breaks here
+print("Thanks for using the program...goodbye!")
+'''
+# While loop that runs 5 times
+
+num = 0
+
+while num  <= 5:
+    print("\U0001f600" * 10)
+    num +=1
+print("Loop has ended!")
+
